f555a717ec8ed0c0c4dcaee3b4dfe01329c254f8
bvengst <Brandon.Engstrom@ttigroupna.com>
2025-10-17 19:53:24 +0000

    Added SGA INT Table

---
+++ cch_dbt_poc/models/int/pricing_form/int_pricing_form__sga_rates.sql
@@ -0,0 +1,16 @@
+SELECT 
+     UPLOAD."material"                                  AS "material"
+    ,UPLOAD."material_description"                      AS "material_description"
+    ,UPLOAD."customer"                                  AS "customer"
+    ,UPLOAD."department"                                AS "department"
+    ,UPLOAD."new_invoice"                               AS "new_invoice"
+    ,UPLOAD."new_retail"                                AS "new_retail"
+    ,UPLOAD."outdoor_standard_cost"                     AS "standard_cost"
+    ,SGA."pricing_form_account"                         AS "pricing_form_account"
+    ,SGA."value"                                        AS "rate"
+    ,SGA."driver"                                       AS "driver"
+FROM {{ ref('stg_pricing_form__pl_test') }} AS UPLOAD
+    LEFT JOIN {{ ref('stg_pricing_form__sga_rates')}} AS SGA
+        ON UPLOAD."material_type" = SGA."material_type"
+        AND UPLOAD."form_of_power" = SGA."form_of_power"
+        AND UPLOAD."category_group" = SGA."category_group"
